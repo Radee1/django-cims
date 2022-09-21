5afd7944ea55c9c6da219d5a505b5750ff047101
Radee1 <roseadee2013@gmail.com>
2022-09-21 23:51:31 +0000

    Adviews for user creation

--- cim_users/views.py
+++ cim_users/views.py
@@ -44,6 +44,19 @@ def logout_user(request):
     return render(request, 'cim_users/user_login.html')
 
 
+def create_user(request):
+    user_name = request.POST['username']
+    user_password = request.POST['password']
+    user_email = request.POST['email']
+    user = User.objects.create_user(user_name, user_email, user_password)
+    # Redirect to a success page.
+    return render(request, 'cim_users/login.html')
+
+def create_user_page(request):
+    # Redirect to a success page.
+    return render(request, 'cim_users/user_create.html')
+
+
 def services(request):
     return render(request, 'cim_users/services.html')
 
