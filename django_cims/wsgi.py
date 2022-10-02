bd53d13da37fc7efaf9137f7e9b7763d8e3596c2
Radee1 <roseadee2013@gmail.com>
2022-10-02 01:30:34 +0000

    solving static file  error

--- django_cims/wsgi.py
+++ django_cims/wsgi.py
@@ -7,10 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cims.settings')
+
+# application = get_wsgi_application()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cims.settings')
 
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(get_wsgi_application())
