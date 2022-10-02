2b7a7c97204f174f4dfe32e4a27fb67e47f7da70
Radee1 <roseadee2013@gmail.com>
2022-10-02 03:50:05 +0000

    Debug static error

--- django_cims/wsgi.py
+++ django_cims/wsgi.py
@@ -7,26 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
-# import os
+import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cims.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cims.settings')
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_cims.settings')
-
-# application = get_wsgi_application()
-
-from whitenoise import WhiteNoise
-
-from django_cims import MyWSGIApp()
-
-application = MyWSGIApp()
-application = WhiteNoise(application, root='cim_users/static/cim_users')
-application.add_files('cim_users/static/cim_users', prefix='staticfiles')
+# from whitenoise import WhiteNoise
