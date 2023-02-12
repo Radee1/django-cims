5b8bf8c9451c173d2aa21e67f2ab5eb0349320d0
Radee1 <roseadee2013@gmail.com>
2023-02-12 10:50:44 +0000

    Changed cim_users.urls path names for home, team

--- cim_users/urls.py
+++ cim_users/urls.py
@@ -4,14 +4,14 @@ from . import views
 # Users urls defined here
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
-    path('login', views.login, name='login'),
+    path('/home/', views.home, name='home'),
+    path('/login/', views.login, name='login'),
     path('login_user', views.login_user, name='login_user'),
     path('logout_user', views.logout_user, name='logout_user'),
     path('create_user_page', views.create_user_page, name='create_user_page'),
     path('create_user', views.create_user, name='create_user'),
-    path('services/', views.services, name='services'),
-    path('team/', views.team, name='team'),
+    path('/services/', views.services, name='services'),
+    path('/team/', views.team, name='team'),
     path('add_member', views.add_member, name='add_member'),
     path('update_member', views.update_member, name='update_member'),
     path('delete_member', views.delete_member, name='delete_member'),
