20f168b57a4a26c0c7ce0e57775afe1fbf360946
Radee1 <roseadee2013@gmail.com>
2023-02-19 18:46:04 +0000

    Added .env file and removed secret key from settings.py

--- django_cims/settings.py
+++ django_cims/settings.py
@@ -19,8 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q3s8*k-)kzu2&fbpr80sq87-y99(s2+(j#=^ij+8&kskv7a%cn'
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env(env_file=str(BASE_DIR) + '/.env')
+
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
