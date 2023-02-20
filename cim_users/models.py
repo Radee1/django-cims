3903a674cdad7cdf88562e3b040cf34e07a5269e
Radee1 <roseadee2013@gmail.com>
2023-02-20 14:45:22 +0000

    ADD CLASS DOCSTRINGS
    Added class docstrings to cim_users.models to clear code validator error.

--- cim_users/models.py
+++ cim_users/models.py
@@ -1,25 +1,34 @@
+"""Django Cims Models."""
 from django.db import models
 
 
 class Usaz(models.Model):
+    """User Model."""
+
     user_ID = models.CharField(max_length=10)
     user_name = models.CharField(max_length=20)
     sign_in_time = models.DateTimeField(auto_now=True)
 
 
 class Patient(models.Model):
+    """Patient Model."""
+
     full_name = models.CharField(max_length=80)
     symptoms = models.CharField(max_length=200)
     time_of_visit = models.DateTimeField(auto_now=True)
 
 
 class Team(models.Model):
+    """Team Model."""
+
     full_name = models.CharField(max_length=80)
     position = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
 
 
 class Diagnosis(models.Model):
+    """Diagnosis Model."""
+
     patient_name = models.CharField(max_length=80)
     test_name = models.CharField(max_length=200)
     diagnosis = models.CharField(max_length=200)
@@ -29,6 +38,8 @@ class Diagnosis(models.Model):
 
 
 class Appointment(models.Model):
+    """Appointment Model."""
+
     patient_name = models.CharField(max_length=80)
     doctor = models.CharField(max_length=200)
     time = models.CharField(max_length=200)
@@ -37,6 +48,8 @@ class Appointment(models.Model):
 
 
 class Medicine(models.Model):
+    """Medicine Model."""
+
     drug_name = models.CharField(max_length=80)
     stock = models.CharField(max_length=200)
     prescribed_to = models.CharField(max_length=200)
