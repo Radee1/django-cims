fab0fc9d72dbe8f9a01279185f1a539e1b454f6f
Radee1 <roseadee2013@gmail.com>
2023-02-21 12:31:27 +0000

    MODIFY APP SETTINGS
    Modified settings.py to solve production error.

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
-os.environ["DATABASE_URL"]= "postgres://yjtzvczeumahvo:482b10cb73629d2437b7166fb6b69044106952e850988e9282d765b4ff5ba6e9@ec2-54-155-129-189.eu-west-1.compute.amazonaws.com:5432/d462tpie46gfsi"
+os.environ["DATABASE_URL"]= "postgres://kleuaszg:ZFllRVewMdPEzwiEbnZhE6rpImTzEoa8@mouse.db.elephantsql.com/kleuaszg"
 os.environ["SECRET_KEY"]="randomSecretKey2024"
 os.environ["CLOUDINARY"]="cloudinary://956678213679386:OrOgsSfkNtd--_PwxfVgMCqy2EE@radee1"
