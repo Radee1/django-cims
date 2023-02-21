22cb0837f5df9d227bfc3fa136a800d83b808deb
Radee1 <roseadee2013@gmail.com>
2023-02-21 12:46:29 +0000

    SOLVED DEPLOYMENT BUG
    Added new database url to  settings.py to solve production error.

--- django_cims/settings.py
+++ django_cims/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'cim_users/templates/cim_users')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["django-cims.herokuapp.com", "https://django-cims.herokuapp.com"]
 
