39f06a9efe9a80c9fb3af41d1af576b5caf7e0ca
Radee1 <roseadee2013@gmail.com>
2023-02-21 12:55:01 +0000

    SOLVED DEPLOYMENT BUG
    Added new database configuration  to  settings.py to solve production error.

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
-os.environ["DATABASE_URL"]= ""
-os.environ["SECRET_KEY"]=""
-os.environ["CLOUDINARY"]=""
+os.environ["DATABASE_URL"]= "postgres://kleuaszg:ZFllRVewMdPEzwiEbnZhE6rpImTzEoa8@mouse.db.elephantsql.com/kleuaszg"
+os.environ["SECRET_KEY"]="randomSecretKey2024"
+os.environ["CLOUDINARY"]="cloudinary://956678213679386:OrOgsSfkNtd--_PwxfVgMCqy2EE@radee1"
