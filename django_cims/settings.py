febce9e1085cf16c19dfa5cb99a1a3a5a813e8c3
Radee1 <roseadee2013@gmail.com>
2023-02-21 11:34:06 +0000

    MODIFY APP SETTINGS
    Modified settings.py to solve production error.

--- django_cims/settings.py
+++ django_cims/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-cims.herokuapp.com", "https://django-cims.herokuapp.com/"]
+ALLOWED_HOSTS = ["django-cims.herokuapp.com", "https://django-cims.herokuapp.com"]
 
 # Application definition
 
