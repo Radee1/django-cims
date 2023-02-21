7c5a09757c8f7d5e57700f63e56d9a867507fd6b
Radee1 <roseadee2013@gmail.com>
2023-02-21 06:05:19 +0000

    ADD NEW URLS
    Added new urls.py and created handler404 html file for when a page is not found.

--- cim_users/urls.py
+++ cim_users/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = "cim_users"
+
 # Users urls defined here
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.home, name='home'),
+    path('home', views.home, name='home'),
     path('login', views.login, name='login'),
     path('login_user', views.login_user, name='login_user'),
     path('logout_user', views.logout_user, name='logout_user'),
     path('create_user_page', views.create_user_page, name='create_user_page'),
     path('create_user', views.create_user, name='create_user'),
-    path('services/', views.services, name='services'),
-    path('team/', views.team, name='team'),
+    path('services', views.services, name='services'),
+    path('team', views.team, name='team'),
     path('add_member', views.add_member, name='add_member'),
     path('update_member', views.update_member, name='update_member'),
     path('delete_member', views.delete_member, name='delete_member'),
@@ -27,8 +29,9 @@ urlpatterns = [
     path('add_treatment', views.add_treatment, name='add_treatment'),
     path('update_treatment', views.update_treatment, name='update_treatment'),
     path('delete_treatment', views.delete_treatment, name='delete_treatment'),
-    path('patients/', views.patients, name='patients/'),
-    path('add_patient/', views.add_patient, name='add_patient/'),
+    path('patients/', views.patients, name='patients'),
+    path('add_patient/', views.add_patient, name='add_patient'),
     path('update_patient', views.update_patient, name='update_patient'),
     path('delete_patient', views.delete_patient, name='delete_patient'),
 ]
+handler404 = 'cim_users.views.handler404'
